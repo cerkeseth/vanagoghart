356e2651d0d7644251eac5236da66e254a5fe501
mertcan <111664019+cerkeseth@users.noreply.github.com>
2025-05-19 12:17:26 +0300

    Initial commit

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,5 +1,5 @@
 // Mainnet contract adresi
-export const NEXT_PUBLIC_CONTRACT_ADDRESS = '0x7e32d38df8f3647bd7976040d46Df0C61B0Ccf3B'
+export const NEXT_PUBLIC_CONTRACT_ADDRESS = '0xaa96cB568F4cefd1B76caDc89Ea663b6F225B971'
 
 // Diğer public konfigürasyonlar buraya eklenebilir
 export const NEXT_PUBLIC_CHAIN_ID = 1480 // Vana Mainnet
